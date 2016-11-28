b2adaf73ccf8fb6c03151ec3b87ad4413f01b981
Nikitosh <npodguzov@yandex.ru>
2016-11-28 19:54:05 +0300

    Add Terminal function

---
+++ src/main/java/com/nikitosh/spbau/model/GameTerminalFunction.java
@@ -0,0 +1,12 @@
+package com.nikitosh.spbau.model;
+
+import burlap.mdp.core.TerminalFunction;
+import burlap.mdp.core.state.State;
+
+public class GameTerminalFunction implements TerminalFunction {
+    @Override
+    public boolean isTerminal(State state) {
+        GameState gameState = (GameState) state;
+        return ((Agent) gameState.object("opponent")).getPlanets().isEmpty();
+    }
+}
