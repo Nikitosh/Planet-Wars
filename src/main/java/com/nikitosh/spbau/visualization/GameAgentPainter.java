400c0632f41dcb6da17117188ad278157d44f6b4
Nikitosh <npodguzov@yandex.ru>
2017-01-30 01:24:23 +0300

    Change choosing color for planet from ifs to static map

--- src/main/java/com/nikitosh/spbau/visualization/GameAgentPainter.java
+++ src/main/java/com/nikitosh/spbau/visualization/GameAgentPainter.java
@@ -1,41 +1,30 @@
 package com.nikitosh.spbau.visualization;
 
 import burlap.mdp.core.oo.state.*;
-import burlap.visualizer.ObjectPainter;
+import burlap.visualizer.*;
 import com.nikitosh.spbau.model.*;
 
 import java.awt.*;
+import java.util.*;
 
 public class GameAgentPainter implements ObjectPainter {
+    private static final Map<String, Color> COLORS = new HashMap<String, Color>() {{
+        put(GameState.NEUTRAL_NAME, Color.YELLOW);
+        put(GameState.AGENT_NAME, Color.GREEN);
+        put(GameState.OPPONENT_NAME, Color.RED);
+    }};
+
     @Override
     public void paintObject(Graphics2D graphics2D, OOState ooState, ObjectInstance objectInstance,
                             float cWidth, float cHeight) {
         Agent agent = (Agent) objectInstance;
-        Color color = Color.BLACK;
-        if (agent.name().equals(GameState.AGENT_NAME)) {
-            color = Color.GREEN;
-        }
-        if (agent.name().equals(GameState.OPPONENT_NAME)) {
-            color = Color.RED;
-        }
-        if (agent.name().equals(GameState.NEUTRAL_NAME)) {
-            color = Color.YELLOW;
-        }
+        Color color = COLORS.get(agent.name());
         graphics2D.setColor(color);
 
         agent.getPlanets().forEach(planet -> {
             int radius = 3 * planet.getSpaceshipsNumber();
             graphics2D.fillOval(planet.getX() - radius / 2, planet.getY() - radius / 2, radius, radius);
-            drawCenteredString(graphics2D, String.valueOf(planet.getSpaceshipsNumber()), planet.getX(), planet.getY());
+            VisualizationUtilities.drawCenteredString(graphics2D, String.valueOf(planet.getSpaceshipsNumber()), planet.getX(), planet.getY(), Color.BLACK);
         });
-    }
-
-    private void drawCenteredString(Graphics2D graphics2D, String text, float x, float y) {
-        Color oldColor = graphics2D.getColor();
-        graphics2D.setColor(Color.BLACK);
-        FontMetrics metrics = graphics2D.getFontMetrics();
-        graphics2D.drawString(text, x - metrics.stringWidth(text) / 2,
-                y - metrics.getHeight() / 2 + metrics.getAscent());
-        graphics2D.setColor(oldColor);
     }
 }
