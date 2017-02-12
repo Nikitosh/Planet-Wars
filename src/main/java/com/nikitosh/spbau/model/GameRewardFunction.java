0a04a32be61afc66d28cc567f0448c014e926b67
Nikitosh <npodguzov@yandex.ru>
2017-02-12 18:16:24 +0300

    Change reward function to the difference between troopers number in new state

--- src/main/java/com/nikitosh/spbau/model/GameRewardFunction.java
+++ src/main/java/com/nikitosh/spbau/model/GameRewardFunction.java
@@ -5,16 +5,9 @@ import burlap.mdp.core.state.State;
 import burlap.mdp.singleagent.model.RewardFunction;
 
 public class GameRewardFunction implements RewardFunction {
-    public static final double INFINITY = 1e9;
-
     @Override
     public double reward(State state, Action action, State newState) {
-        GameState gameState = (GameState) state;
-        if (!gameState.isApplicable((MoveAction) action)) {
-            return -INFINITY;
-        }
-        GameState newGameState = (GameState) newState;
-        return getEvaluation(newGameState) - getEvaluation(gameState);
+        return getEvaluation((GameState) newState);
     }
 
     private double getEvaluation(GameState gameState) {
@@ -23,7 +16,6 @@ public class GameRewardFunction implements RewardFunction {
     }
 
     private double getEvaluation(Agent agent) {
-        //return agent.getPlanets().stream().mapToInt(Planet::getSpaceshipsNumber).sum();
-        return agent.getPlanets().stream().mapToInt(planet -> planet.getSpaceshipsNumber() + Planet.MAX_CAPACITY).sum();
+        return agent.getPlanets().stream().mapToInt(Planet::getSpaceshipsNumber).sum();
     }
 }
