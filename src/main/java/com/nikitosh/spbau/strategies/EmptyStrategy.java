f87525a964e44a91774bea8a20737b4988a54ae6
Nikitosh <npodguzov@yandex.ru>
2017-02-12 18:06:59 +0300

    Change empty strategy for making only allowed actions

--- src/main/java/com/nikitosh/spbau/strategies/EmptyStrategy.java
+++ src/main/java/com/nikitosh/spbau/strategies/EmptyStrategy.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 public class EmptyStrategy implements Strategy {
     @Override
     public Action getAction(GameState state) {
-        String name = state.objects().stream().flatMap(agent -> ((Agent) agent).getPlanets().stream())
+        String name = ((Agent) state.object(GameState.OPPONENT_NAME)).getPlanets().stream()
                 .collect(Collectors.toList()).get(0).name();
         return new MoveAction(name, name, 0);
     }
