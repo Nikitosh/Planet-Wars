d83027ade7fd95cce4286e07ebefa10b7a01a965
Nikitosh <npodguzov@yandex.ru>
2017-02-16 01:49:29 +0300

    Add playing against GreedyAttackStrategy in Main

--- src/main/java/com/nikitosh/spbau/Main.java
+++ src/main/java/com/nikitosh/spbau/Main.java
@@ -1,5 +1,7 @@
 package com.nikitosh.spbau;
 
+import burlap.behavior.policy.*;
+import burlap.behavior.singleagent.*;
 import burlap.behavior.singleagent.learning.tdmethods.*;
 import burlap.mdp.core.state.*;
 import burlap.mdp.singleagent.common.*;
@@ -8,32 +10,37 @@ import burlap.mdp.singleagent.oo.*;
 import burlap.statehashing.*;
 import burlap.visualizer.*;
 import com.nikitosh.spbau.model.*;
+import com.nikitosh.spbau.strategies.*;
 import com.nikitosh.spbau.visualization.*;
 
 import java.io.*;
+import java.util.*;
 
 public final class Main {
     private static final String MAP_FILE_PATH = "src\\test\\resources\\map.txt";
     private static final double GAMMA = 0.99;
     private static final double LEARNING_RATE = 1;
     private static final int TRIALS_NUMBER = 5;
-    private static final int TRIAL_LENGTH = 2000;
+    private static final int TRIAL_LENGTH = 10000;
     private static final int CHART_WIDTH = 500;
     private static final int CHART_HEIGHT = 250;
     private static final int COLUMNS_NUMBER = 2;
     private static final int MAXIMUM_WINDOW_HEIGHT = 1000;
-    private static final long FRAME_DELAY = 1000;
+    private static final long FRAME_DELAY = 2000;
 
     private Main() {}
 
     public static void main(String[] args) throws FileNotFoundException {
         World world = World.readFromFile(new File(MAP_FILE_PATH));
+        world.getActions().forEach(System.out::println);
         State initialState = world.getState();
-        GameDomainGenerator domainGenerator = new GameDomainGenerator(world.getActions());
+        GameDomainGenerator domainGenerator = new GameDomainGenerator(world.getActions(),
+                new RandomStrategy(world.getActions()));
         OOSADomain domain = domainGenerator.generateDomain();
         SimulatedEnvironment environment = new SimulatedEnvironment(domain, initialState);
         HashableStateFactory hashingFactory = new ReflectiveHashableStateFactory();
 
+        /*
 
         Visualizer visualizer = new GameVisualizer(world.getEdges()).getVisualizer();
         VisualActionObserver observer = new VisualActionObserver(visualizer);
@@ -45,31 +52,63 @@ public final class Main {
         QLearning agent = new QLearning(domain, GAMMA, hashingFactory, 0, LEARNING_RATE);
         agent.runLearningEpisode(environment);
 
+        */
+
         /**
          * This part can be used to run many learning episodes on current model.
          */
 
-        /*
+
         List<Episode> episodes = new ArrayList<>();
         QLearning agent = new QLearning(domain, GAMMA, hashingFactory, 0, LEARNING_RATE);
         for (int i = 0; i < TRIAL_LENGTH; i++) {
+            System.out.println("Episode #" + i);
             Episode e = agent.runLearningEpisode(environment);
-            e.write("results/" + i);
-            episodes.add(e);
+            //episodes.add(e);
             environment.resetEnvironment();
-            //e.rewardSequence.stream().forEach(System.out::println);
-            //System.out.println();
-            //System.out.println(e.rewardSequence.stream().mapToDouble(Double::doubleValue).sum());
-            //System.out.println(e.maxTimeStep());
+            System.out.println(e.rewardSequence.stream().mapToDouble(Double::doubleValue).sum());
+            System.out.println(e.maxTimeStep());
         }
-        new EpisodeSequenceVisualizer(visualizer, domain, episodes);
+        agent.setLearningPolicy(new GreedyQPolicy(agent));
+        /*
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Episode ###" + i);
+            Episode e = agent.runLearningEpisode(environment);
+            //episodes.add(e);
+            environment.resetEnvironment();
+            System.out.println(e.rewardSequence.stream().mapToDouble(Double::doubleValue).sum());
+            System.out.println(e.maxTimeStep());
+        }
         */
+
+        Visualizer visualizer = new GameVisualizer(world.getEdges()).getVisualizer();
+        VisualActionObserver observer = new VisualActionObserver(visualizer);
+        observer.setRepaintOnActionInitiation(true);
+        observer.setRepaintStateOnEnvironmentInteraction(false);
+        observer.initGUI();
+        observer.setFrameDelay(FRAME_DELAY);
+        environment.addObservers(observer);
+        agent.runLearningEpisode(environment);
+
+        /*
+        agent.setLearningPolicy(new GreedyQPolicy(agent));
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("Episode ###" + i);
+            Episode e = agent.runLearningEpisode(environment);
+            environment.resetEnvironment();
+            System.out.println(e.rewardSequence.stream().mapToDouble(Double::doubleValue).sum());
+            System.out.println(e.maxTimeStep());
+        }
+        */
+        
+        //new EpisodeSequenceVisualizer(visualizer, domain, episodes);
 
         /**
          * This part can be used for making learning curves on plot.
          */
 
         /*
+
         LearningAgentFactory qLearningFactory = new LearningAgentFactory() {
             @Override
             public String getAgentName() {
@@ -78,7 +117,19 @@ public final class Main {
 
             @Override
             public LearningAgent generateAgent() {
-                return new QLearning(domain, GAMMA, hashingFactory, 0, LEARNING_RATE);
+                QLearning agent = new QLearning(domain, GAMMA, hashingFactory, 0, LEARNING_RATE);
+
+                for (int i = 0; i < TRIAL_LENGTH; i++) {
+                    agent.runLearningEpisode(environment);
+                    environment.resetEnvironment();
+                    if (i % 10 == 0) {
+                        System.out.println(i);
+                    }
+                }
+                agent.setLearningPolicy(new GreedyQPolicy(agent));
+
+                //agent.setLearningPolicy(new EpsilonGreedy(agent, 0.1D));
+                return agent;
             }
         };
 
@@ -88,10 +139,11 @@ public final class Main {
         exp.setUpPlottingConfiguration(CHART_WIDTH, CHART_HEIGHT, COLUMNS_NUMBER, MAXIMUM_WINDOW_HEIGHT,
                 TrialMode.MOST_RECENT_AND_AVERAGE,
                 PerformanceMetric.CUMULATIVE_STEPS_PER_EPISODE,
-                PerformanceMetric.AVERAGE_EPISODE_REWARD);
+                PerformanceMetric.CUMULATIVE_REWARD_PER_EPISODE);
 
         exp.startExperiment();
         exp.writeStepAndEpisodeDataToCSV("expData");
+
         */
 
         /**
