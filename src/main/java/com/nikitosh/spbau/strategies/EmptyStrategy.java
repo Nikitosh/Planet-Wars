8d3b797581838dac3be835c5cf7843608b12f94b
Nikitosh <npodguzov@yandex.ru>
2017-02-16 01:46:34 +0300

    Add getActions() method in EmptyStrategy

--- src/main/java/com/nikitosh/spbau/strategies/EmptyStrategy.java
+++ src/main/java/com/nikitosh/spbau/strategies/EmptyStrategy.java
@@ -1,15 +1,21 @@
 package com.nikitosh.spbau.strategies;
 
-import burlap.mdp.core.action.Action;
+import burlap.mdp.core.action.*;
 import com.nikitosh.spbau.model.*;
 
-import java.util.stream.Collectors;
+import java.util.*;
+import java.util.stream.*;
 
 public class EmptyStrategy implements Strategy {
     @Override
-    public Action getAction(GameState state) {
+    public List<ActionProbability> getActions(GameState state) {
         String name = ((Agent) state.object(GameState.OPPONENT_NAME)).getPlanets().stream()
                 .collect(Collectors.toList()).get(0).name();
-        return new MoveAction(name, name, 0);
+        return Collections.singletonList(new ActionProbability(new MoveAction(name, name, 0), 1.));
+    }
+
+    @Override
+    public Action getAction(GameState state) {
+        return getActions(state).get(0).getAction();
     }
 }
