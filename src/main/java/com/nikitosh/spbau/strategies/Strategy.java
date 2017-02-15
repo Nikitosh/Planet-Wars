ea27b2795d0e85201bce1fcb36ec884ddf8ca18c
Nikitosh <npodguzov@yandex.ru>
2017-02-16 01:46:00 +0300

    Add getActions() in Strategy interface (for strategies using random)

--- src/main/java/com/nikitosh/spbau/strategies/Strategy.java
+++ src/main/java/com/nikitosh/spbau/strategies/Strategy.java
@@ -1,8 +1,11 @@
 package com.nikitosh.spbau.strategies;
 
-import burlap.mdp.core.action.Action;
-import com.nikitosh.spbau.model.GameState;
+import burlap.mdp.core.action.*;
+import com.nikitosh.spbau.model.*;
+
+import java.util.*;
 
 public interface Strategy {
+    List<ActionProbability> getActions(GameState state);
     Action getAction(GameState state);
 }
