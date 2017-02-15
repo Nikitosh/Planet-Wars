a8fe4c9e1fde4b3dce97ae039974435bc32648bf
Nikitosh <npodguzov@yandex.ru>
2017-02-16 01:48:25 +0300

    Add GreedyAttackStrategy which is just attacking planets if it's possible

---
+++ src/main/java/com/nikitosh/spbau/strategies/GreedyAttackStrategy.java
@@ -0,0 +1,33 @@
+package com.nikitosh.spbau.strategies;
+
+import burlap.mdp.core.action.*;
+import com.nikitosh.spbau.model.*;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class GreedyAttackStrategy implements Strategy {
+    private Random random = new Random();
+    private List<Action> actions;
+
+    public GreedyAttackStrategy(List<Action> actions) {
+        this.actions = actions;
+    }
+
+    @Override
+    public List<ActionProbability> getActions(GameState state) {
+        Agent opponent = (Agent) state.object(GameState.OPPONENT_NAME);
+        List<Action> applicableActions = actions.stream().filter(action -> {
+            MoveAction moveAction = (MoveAction) action;
+            return moveAction.isApplicable(state, opponent)
+                    && (moveAction.isAttacking(state, opponent) || moveAction.isWaiting());
+        }).collect(Collectors.toList());
+        return applicableActions.stream().map(action -> new ActionProbability(action, 1. / applicableActions.size()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Action getAction(GameState state) {
+        List<ActionProbability> applicableActions = getActions(state);
+        return applicableActions.get(random.nextInt(applicableActions.size())).getAction();
+    }}
